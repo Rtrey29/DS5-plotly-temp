794f9bacdc2c0fa094f305b54064191157406a4e
Richard Tregaro <rcloud29@icloud.com>
2019-08-01 08:18:11 -0700

    project2_update

--- pages/index.py
+++ pages/index.py
@@ -10,17 +10,17 @@ from app import app
 """
 https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
 
-Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has 
+Layout in Bootstrap is controlled using the grid system. The Bootstrap grid has
 twelve columns.
 
-There are three main layout components in dash-bootstrap-components: Container, 
+There are three main layout components in dash-bootstrap-components: Container,
 Row, and Col.
 
 The layout of your app should be built as a series of rows of columns.
 
-We set md=4 indicating that on a 'medium' sized or larger screen each column 
-should take up a third of the width. Since we don't specify behaviour on 
-smallersize screens Bootstrap will allow the rows to wrap so as not to squash 
+We set md=4 indicating that on a 'medium' sized or larger screen each column
+should take up a third of the width. Since we don't specify behaviour on
+smallersize screens Bootstrap will allow the rows to wrap so as not to squash
 the content.
 """
 
@@ -28,18 +28,16 @@ column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
-            ## Value Proposition
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
+            ## Predict the price of a 🏠 in King County WA
 
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
+            This tool will help you to determine the price of a home, so you can better determine the size of house that is within your price range.
 
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
+
 
             """
         ),
-        dcc.Link(dbc.Button('Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Get home prices', color='primary'), href='/predictions')
     ],
     md=4,
 )
@@ -48,10 +46,6 @@ gapminder = px.data.gapminder()
 fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
            hover_name="country", log_x=True, size_max=60)
 
-column2 = dbc.Col(
-    [
-        dcc.Graph(figure=fig),
-    ]
-)
+column2 = dbc.Col(html.Img(src='assets/prices.jpg', className='img-fluid'))
 
 layout = dbc.Row([column1, column2])
--- pages/predictions.py
+++ pages/predictions.py
@@ -3,27 +3,140 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from joblib import load
+import pandas as pd
 
 from app import app
+
+pipeline = load('assets/pipeline.joblib')
 
 column1 = dbc.Col(
     [
         dcc.Markdown(
             """
-        
+
             ## Predictions
 
 
-            """
+            """,className='mb-5',
         ),
+        dcc.Markdown('#### Living space in sqft'),
+        dcc.Slider(
+        id = 'sqft_living',
+        min =300,
+        max =7000,
+        step=250,
+        value=1500,
+        marks={n: str(n) for n in range(500, 7000, 1000)},
+        className='mb-5',
+        ),
+        dcc.Markdown('#### Number of Bedrooms'),
+        dcc.Dropdown(
+        id='bedrooms',
+        options = [
+            {'label':'0', 'value':0},
+            {'label':'1', 'value':1},
+            {'label':'2', 'value':2},
+            {'label':'3', 'value':3},
+            {'label':'4', 'value':4},
+            {'label':'5', 'value':5},
+            {'label':'6', 'value':6},
+            {'label':'7', 'value':7},
+            {'label':'8', 'value':8},
+            {'label':'9', 'value':9},
+            {'label':'10', 'value':10},
+        ],
+        value = 1,
+        className='mb-5',
+        ),
+        dcc.Markdown('#### Number of Bathrooms'),
+        dcc.Dropdown(
+        id='bathrooms',
+        options = [
+            {'label':'1', 'value':1},
+            {'label':'1.5', 'value':1.5},
+            {'label':'2', 'value':2},
+            {'label':'2.5', 'value':2.5},
+            {'label':'3', 'value':3},
+            {'label':'3.5', 'value':3.5},
+            {'label':'4', 'value':4},
+            {'label':'4.5', 'value':4.5},
+            {'label':'5', 'value':5},
+            {'label':'5.5', 'value':5.5},
+            {'label':'6', 'value':6},
+
+        ],
+        value = 1,
+        className='mb-5',
+        ),
+
     ],
     md=4,
 )
 
 column2 = dbc.Col(
     [
-        
+
+
+        dcc.Markdown('#### Condition'),
+        dcc.Markdown('An index from 1-5 on the condition of the property 1=poor/5=excellent'),
+        dcc.RadioItems(
+        id= 'condition',
+        options=[
+            {'label': '1', 'value': 1},
+            {'label': '2', 'value': 2},
+            {'label': '3', 'value': 3},
+            {'label': '4', 'value': 4},
+            {'label': '5', 'value': 5},
+        ],
+        value = 3,
+        className='mb-5',
+        ),
+        dcc.Markdown('#### Floors'),
+        dcc.Dropdown(
+        id='floors',
+        options = [
+            {'label':'1', 'value':1},
+            {'label':'2', 'value':2},
+            {'label':'3', 'value':3},
+        ],
+        value=1,
+        className='mb-5',
+        ),
+        dcc.Markdown('#### Waterfront Property'),
+        dcc.Dropdown(
+        id = 'waterfront',
+        options=[
+            {'label': 'Yes', 'value' : 1},
+            {'label': 'No', 'value' : 0},
+        ],
+        value=0,
+        className='mb-5',
+        ),
+
+    ],
+    md=4,
+)
+column3 = dbc.Col(
+    [
+        html.H2('Price of home', className='mb-5'),
+        html.Div(id='prediction-content', className='lead'),
     ]
 )
+import pandas as pd
 
-layout = dbc.Row([column1, column2])
+@app.callback(
+    Output('prediction-content', 'children'),
+    [Input('year', 'value'), Input('continent', 'value')],
+)
+def predict(beds,baths,sqft,floor, water, cond, grade, yr):
+    df = pd.DataFrame(
+        columns=['bedrooms', 'bathrooms','sqft_living','floors', 'waterfront', 'condition', 'grade', 'yr_built'],
+        data=[[ beds, baths, sqft, floor, water, cond, 7, yr]]
+    )
+    y_pred_log=pipeline.predict(df)[0]
+    y_pred = np.expm1(y_pred_log)
+
+    return y_pred
+
+layout = dbc.Row([column1, column2, column3])
